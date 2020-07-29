58c13b38bd543906f915fbc549860e904faad96b
yevhenl <jmen7070@gmail.com>
2020-07-29 09:44:50 +0300

    batches

--- src/main/java/com/example/generator/SentencesGeneratorMain.java
+++ src/main/java/com/example/generator/SentencesGeneratorMain.java
@@ -1,5 +1,6 @@
 package com.example.generator;
 
+import com.google.common.collect.Lists;
 import org.deeplearning4j.api.storage.StatsStorage;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.ui.api.UIServer;
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.text.BreakIterator;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -22,6 +24,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class SentencesGeneratorMain {
+
+    private static final int BATCH_SIZE = 8;
 
     private static final int startIndex = 0;
     private static final int endIndex = 1;
@@ -43,12 +47,17 @@ public class SentencesGeneratorMain {
         uiServer.attach(ganStatsStorage);
         graph.setListeners(new StatsListener( ganStatsStorage, 20));
 
-        AtomicInteger iteration = new AtomicInteger(0);
-        Files.lines(file.toPath())
+        List<DataSet> dataSets = Files.lines(file.toPath())
                 .map(line -> line.split("\t")[1])
                 .map(line -> trim(line, 50))
                 .map(line -> line.chars().mapToObj(uniqueCharsIndices::get).collect(Collectors.toList()))
                 .map(seq -> toDataSet(seq, uniqueCharsIndices.size() + 2))
+                .collect(Collectors.toList());
+
+        AtomicInteger iteration = new AtomicInteger(0);
+        Lists.partition(dataSets, BATCH_SIZE)
+                .stream()
+                .map(DataSet::merge)
                 .peek(b -> {
                     if(iteration.incrementAndGet()%1000 == 0 ){
                         try {
@@ -62,16 +71,22 @@ public class SentencesGeneratorMain {
     }
 
     private static DataSet toDataSet(List<Integer> charsSeq, int uniqueCharsCount) {
-        List<Integer> featuresSeq = new ArrayList<>();
-        featuresSeq.add(startIndex);
-        featuresSeq.addAll(charsSeq);
+        int maxLength = 51;
 
-        List<Integer> labelsSeq = new ArrayList<>();
-        labelsSeq.addAll(charsSeq);
-        labelsSeq.add(endIndex);
+        List<Integer> featuresSeq = new ArrayList<>(Collections.nCopies(maxLength, endIndex));
+        featuresSeq.set(0, startIndex);
+        for (int i = 0; i < charsSeq.size(); i++) {
+            featuresSeq.set(i+1, charsSeq.get(i));
+        }
+
+        List<Integer> labelsSeq = new ArrayList<>(Collections.nCopies(maxLength, endIndex));
+        for (int i = 0; i < charsSeq.size(); i++) {
+            labelsSeq.set(i, charsSeq.get(i));
+        }
+        labelsSeq.set(charsSeq.size(), endIndex);
 
         INDArray features = toINDArray(featuresSeq);
-        Pair<INDArray, INDArray> labelsWithMasks = toMatrix(labelsSeq, uniqueCharsCount, 51);
+        Pair<INDArray, INDArray> labelsWithMasks = toMatrix(labelsSeq, uniqueCharsCount, maxLength);
 
         return new DataSet(features, labelsWithMasks.getFirst(), labelsWithMasks.getSecond(), labelsWithMasks.getSecond());
     }
