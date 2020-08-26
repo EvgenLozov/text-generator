684106d273cebf1114befddf7ad00b213c86f9ce
o_ins <oor1@ukr.net>
2020-08-26 17:33:00 +0300

    fukin done

--- src/main/java/com/example/corrector/WordsCorrectorModel.java
+++ src/main/java/com/example/corrector/WordsCorrectorModel.java
@@ -1,7 +1,17 @@
 package com.example.corrector;
 
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.graph.rnn.LastTimeStepVertex;
+import org.deeplearning4j.nn.conf.layers.EmbeddingSequenceLayer;
+import org.deeplearning4j.nn.conf.layers.LSTM;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
 import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +22,45 @@ public class WordsCorrectorModel {
     }
 
     public static ComputationGraph build(int uniqueCharsCount) {
-        //TODO
-        return null;
+        int charEmbedding = 128;
+        double learningRate = 0.001;
+
+        int tbpttLength = 50;
+
+        ComputationGraphConfiguration config = new NeuralNetConfiguration.Builder()
+                .seed(12345)
+                .weightInit(WeightInit.XAVIER)
+                .updater(new Adam(learningRate, 0, 0.99, 1e-8))
+                .graphBuilder()
+                .addInputs("sequence")
+                .addLayer("noisy_input", new EmbeddingSequenceLayer.Builder()
+                        .hasBias(false)
+                        .activation(Activation.IDENTITY)
+                        .nIn(uniqueCharsCount)
+                        .nOut(charEmbedding)
+                        .build(), "sequence"
+                )
+                .appendLayer("lstm", new LSTM.Builder()
+                        .nIn(charEmbedding)
+                        .nOut(charEmbedding)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation(Activation.TANH)
+                        .build()
+                )
+                .appendVertex("last_time_step", new LastTimeStepVertex("sequence"))
+                .appendLayer("output", new RnnOutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
+                        .activation(Activation.SOFTMAX)
+                        .weightInit(WeightInit.XAVIER)
+                        .nIn(128)
+                        .nOut(uniqueCharsCount)
+                        .build()
+                )
+//                .backpropType(BackpropType.TruncatedBPTT)
+//                .tBPTTForwardLength(tbpttLength)
+//                .tBPTTBackwardLength(tbpttLength)
+                .setOutputs("output")
+                .build();
+
+        return new ComputationGraph(config);
     }
 }
